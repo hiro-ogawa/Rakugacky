8317447bd449ce08fbfe9c75c7e55d5e52f992f9
Hiro Ogawa <pooh_mtb@hotmail.com>
2017-05-28 01:37:04 +0900

    add LoadScene by tap

---
+++ Assets/CheckTouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0)
+		{
+			Debug.Log("tap");
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase == TouchPhase.Began)
+			{
+				// タッチ開始
+				SceneManager.LoadScene("VuforiaOgawaScene");
+			}
+			else if (touch.phase == TouchPhase.Moved)
+			{
+				// タッチ移動
+				SceneManager.LoadScene("VuforiaOgawaScene");
+			}
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				// タッチ終了
+				SceneManager.LoadScene("VuforiaOgawaScene");
+			}
+		}
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("mouse");
+			SceneManager.LoadScene("VuforiaOgawaScene");
+    }
+	}
+}
