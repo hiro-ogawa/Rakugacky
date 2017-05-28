dc440a796781146b97aac74c0dbfc2d61bca8df3
takafumi matsudome <ckb.matu@gmail.com>
2017-05-28 11:04:21 +0900

    松留分

--- Assets/CameraFocusController.cs
+++ Assets/CameraFocusController.cs
@@ -5,6 +5,8 @@ using Vuforia;
 public class CameraFocusController : MonoBehaviour {
 
     private bool mVuforiaStarted = false;
+	private Vector3 touchStartPos;
+	private Vector3 touchEndPos;
 
     void Start () 
     {
@@ -12,8 +14,12 @@ public class CameraFocusController : MonoBehaviour {
 
         if (vuforia != null)
             vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+		Flick ();
     }
 
+	void Update (){
+		Flick ();
+	}
     private void StartAfterVuforia()
     {
         mVuforiaStarted = true;
@@ -47,4 +53,72 @@ public class CameraFocusController : MonoBehaviour {
             Debug.Log("this device doesn't support auto focus");
         }
     }
+
+	void Flick(){
+		if (Input.GetKeyDown(KeyCode.Mouse0)){
+			touchStartPos = new Vector3(Input.mousePosition.x,
+				Input.mousePosition.y,
+				Input.mousePosition.z);
+		}
+
+		if (Input.GetKeyUp(KeyCode.Mouse0)){
+			touchEndPos = new Vector3(Input.mousePosition.x,
+				Input.mousePosition.y,
+				Input.mousePosition.z);
+			GetDirection();
+		}
+	}
+
+	void GetDirection(){
+		float directionX = touchEndPos.x - touchStartPos.x;
+		float directionY = touchEndPos.y - touchStartPos.y;
+		string Direction = "";
+
+		if (Mathf.Abs(directionY) < Mathf.Abs(directionX)){
+			if (30 < directionX){
+				//右向きにフリック
+				Direction = "right";
+
+			}else if (-30 > directionX){
+				//左向きにフリック
+				Direction = "left";
+			}
+		}else if (Mathf.Abs(directionX)<Mathf.Abs(directionY)){
+			if (30 < directionY){
+				//上向きにフリック
+				Direction = "up";
+			}else if (-30 > directionY){
+				//下向きのフリック
+				Direction = "down";
+			}
+		}else{
+			//タッチを検出
+			Direction = "touch";
+		}
+		Debug.Log("Direction:"+ Direction );
+
+		switch (Direction){
+		case "up":
+			//上フリックされた時の処理
+			break;
+
+		case "down":
+			//下フリックされた時の処理
+			break;
+
+		case "right":
+			//右フリックされた時の処理
+			break;
+
+		case "left":
+			//左フリックされた時の処理
+			break;
+
+		case "touch":
+			//タッチされた時の処理
+			break;
+		}
+
+	}
+
 }
